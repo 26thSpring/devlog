6af8ea38ad0cee2ea3709b6c74d962dc902779dc
26thSpring <foelsk56@gmail.com>
2018-08-21 17:08:18 +0900

    devlog test version 1.4

--- src/components/main/posts/Post.js
+++ src/components/main/posts/Post.js
@@ -1,33 +1,39 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './Post.scss';
+import defaultProfileImage from 'img/defaultProfileImage.svg';
+import defaultThumbnailImage from 'img/defaultThumbnailImage.svg';
 
 class Post extends Component {
    render() {
       return (
          <div className="Post">
-            <Link className="Post__thumbnail" to="/">
-               <svg viewBox="0 0 5rem 4rem">
-                  <use xlinkHref="img/defaultProfileImage.svg" />
-               </svg>
-               <div className="whiteSpace" />
-            </Link>
-            <div className="Post__head">
-               <span className="Post__head__name">스물여섯번째 봄</span>
-               <Link to="/">
-                  <span className="Post__head__title">취업 좀 가즈아</span>
+            <div className="Post__wrappper">
+               <Link className="Post__thumbnail" to="/">
+                  <div className="whiteSpace" />
+                  <object type="image/svg+xml" data={defaultThumbnailImage}>
+                     현재 브라우저는 iframe을 지원하지 않습니다.
+                  </object>
                </Link>
-               <span className="Post__head__regdate">1일 전 · 2개의 댓글</span>
-               <div className="Post__head__profile">
-                  <img
-                     className="Post__head__profileImage"
-                     src="defaultProfileImage.png"
-                     alt="defaultImage"
-                  />
+               <div className="Post__head">
+                  <span className="Post__head__name">스물여섯번째 봄</span>
+                  <Link to="/">
+                     <span className="Post__head__title">취업 좀 가즈아</span>
+                  </Link>
+                  <span className="Post__head__regdate">
+                     1일 전 · 2개의 댓글
+                  </span>
+                  <div className="Post__head__profile">
+                     <object type="image/svg+xml" data={defaultProfileImage}>
+                        현재 브라우저는 iframe을 지원하지 않습니다.
+                     </object>
+                  </div>
                </div>
-            </div>
-            <div className="Post__content">
-               사람인 ok 잡코리아 ok 인크루트 ok 게임잡 ok 로켓펀치 ok
+               <div className="Post__content">
+                  <span>
+                     사람인 ok 잡코리아 ok 인크루트 ok 게임잡 ok 로켓펀치 ok
+                  </span>
+               </div>
             </div>
          </div>
       );
